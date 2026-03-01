d5d7cf86c774ac4172fc5bc3e5652355b2111d91
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-01 15:54:29 +0530

    Added Second largest element from the array

---
+++ src/arrays/SecondLargestElement.java
@@ -0,0 +1,48 @@
+package arrays;
+import java.util.Scanner;
+
+public class SecondLargestElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        if (n < 2) {
+            System.out.println("Second largest element does not exist.");
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("No distinct second largest element.");
+        } else {
+            System.out.println("Second Largest Element: " + secondLargest);
+        }
+
+        sc.close();
+    }
+}
