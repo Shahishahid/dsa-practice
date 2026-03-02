3ab34df580c9cfd6d89dd1b71aa3cad0e05810d7
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-02 20:03:39 +0530

    Add comments to SumOfEvenIndexElements for better readability

--- src/conditions/SumOfEvenIndexElements.java
+++ src/conditions/SumOfEvenIndexElements.java
@@ -1,19 +1,23 @@
 package conditions;
 import java.util.Scanner;
 
-public class SumEvenNumbers {
-    public static void main(String[] args) {
+public class SumOfEvenIndexElements
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size: ");
         int n = sc.nextInt();
         int[] a = new int[n];
         System.out.println("Enter elements: ");
 
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++)
+        {
             a[i] = sc.nextInt();
         }
         int sum = 0;
-        for (int i = 0; i < n; i=i+2) {
+        for (int i = 0; i < n; i=i+2)//simplified version of processing even index without if statement.
+        {
             //if (i % 2 == 0) {
                 sum = sum + a[i];
 
