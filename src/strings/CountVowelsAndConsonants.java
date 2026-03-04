f03dcaef427e68b6ebfccb46bd3e9813951af6d4
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-04 06:53:26 +0530

    Added code to count the vowels and consonants

---
+++ src/strings/CountVowelsAndConsonants.java
@@ -0,0 +1,31 @@
+package strings;
+import java.util.Scanner;
+public class CountVowelsAndConsonants {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the string: ");
+        String str=sc.nextLine().toLowerCase(); //LOWER CASE TO HANDLE BOTH A AND a.
+        int vowels=0;
+        int consonants=0;
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            if(ch>='a'&&ch<='z')
+            {
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                {
+                    vowels++;
+                }
+                else {
+                    consonants++;
+
+                }
+            }
+
+        }
+        System.out.println("Vowels Count: "+vowels);
+        System.out.println("Consonants Count: "+consonants);
+    }
+}
