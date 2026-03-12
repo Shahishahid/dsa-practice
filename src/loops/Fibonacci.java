2b09cc58ff6fe5b177fcfdf421ebf8e6e2a97d86
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-12 22:40:25 +0530

    Added a code to find the factorial of a given number

---
+++ src/loops/Fibonacci.java
@@ -0,0 +1,21 @@
+package loops;
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        int first=0;
+        int second=1;
+        System.out.print(+first+" "+second+" ");
+        for(int i=3;i<=n;i++)
+        {
+            int next=first+second;
+            System.out.print(next+" ");
+            first=second;
+            second=next;
+
+        }
+    }
+}
