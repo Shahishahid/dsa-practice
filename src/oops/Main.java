3908443ff3297822492b6d649d496fdd942652f4
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-15 10:38:17 +0530

    Added code to demonstrate object creation and function call

---
+++ src/oops/Main.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        ClassExample c1=new ClassExample();
+        c1.id=101;
+        c1.name="Shah";
+        c1.display();
+    }
+}
