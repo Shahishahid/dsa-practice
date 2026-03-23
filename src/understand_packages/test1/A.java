2a65f02529d6f8bc0a88f4a6dded405652cc6f72
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-23 08:27:47 +0530

    Created a test1 package to understand and import it in another package to access

---
+++ src/understand_packages/test1/A.java
@@ -0,0 +1,10 @@
+package understand_packages.test1;
+
+import java.awt.color.ICC_ColorSpace;
+
+public class A {
+
+    public void printMessage(){
+        System.out.println("Hllo Broz");
+    }
+}
