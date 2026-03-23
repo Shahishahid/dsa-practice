712c3c1acad076881c64fcbfdc954edb70331342
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-23 08:29:47 +0530

    Created a test2 package that imports class A of package test1 to access member functions

---
+++ src/understand_packages/test2/B.java
@@ -0,0 +1,11 @@
+package understand_packages.test2;
+import understand_packages.test1.A;
+
+class B {
+    public static void main(String[] args)
+    {
+        A a=new A();
+        a.printMessage();
+
+    }
+}
