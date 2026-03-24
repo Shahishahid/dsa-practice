ca788177010f30ef99bb48940d17662a30da55cb
Shahid Ahmed <shahidmca1@gmail.com>
2026-03-24 11:46:46 +0530

    Add Vehicle class as base class for inheritance with encapsulated speed and common behavior

---
+++ src/oops/inheritance/in1/Vehicle.java
@@ -0,0 +1,14 @@
+package oops.inheritance.in1;
+
+public class Vehicle {
+    private int speed=600;
+
+    public int getSpeed()
+    {
+        return speed;
+    }
+    public void start()
+    {
+        System.out.println("Vehicle is Started.");
+    }
+}
