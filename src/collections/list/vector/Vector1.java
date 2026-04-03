b82251d5174f057927e212d7d461ec469dd7cd25
Shahid Ahmed <shahidmca1@gmail.com>
2026-04-03 11:37:17 +0530

    Added a code to demonstrate the basic working of a Vector

---
+++ src/collections/list/vector/Vector1.java
@@ -0,0 +1,15 @@
+package collections.list.vector;
+import java.util.*;
+
+public class Vector1 {
+    public static void main(String[] args) {
+
+        Vector<Integer> v = new Vector<>();
+
+        v.add(10);
+        v.add(20);
+        v.add(30);
+
+        System.out.println(v);
+    }
+}
